08d1eca137551c53dd400258b8e0914fbf7b1580
luohuidong <luohuidong01@126.com>
2023-12-12 01:28:12 +0800

    feat: define the "build" command

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-const Webpack = require("webpack");
+const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const { Command } = require("commander");
 
 const packageJson = require("../package.json");
 const webpackDevConfig = require("../lib/webpack.dev.js");
+const webpackProdConfig = require("../lib/webpack.prod.js");
 
 const program = new Command();
 program
@@ -14,12 +15,40 @@ program
   .version(packageJson.version);
 
 program.command("dev").action(() => {
-  const compiler = Webpack(webpackDevConfig);
+  const compiler = webpack(webpackDevConfig);
   const server = new WebpackDevServer(webpackDevConfig.devServer, compiler);
   const runServer = async () => {
     await server.start();
   };
   runServer();
+});
+
+program.command("build").action(() => {
+  webpack(webpackProdConfig, (err, stats) => {
+    // https://webpack.js.org/api/node/#error-handling
+
+    // The err object will only contain webpack-related issues, such as misconfiguration
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+
+      return;
+    }
+
+    const info = stats.toJson();
+
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
+
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings);
+    }
+
+    console.log(stats.toString());
+  });
 });
 
 program.parse();
