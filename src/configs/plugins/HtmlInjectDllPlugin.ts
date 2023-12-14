bcf93ed814e2a36974a62f2b15351687a9b1ce45
luohuidong <luohuidong01@126.com>
2023-12-14 15:38:48 +0800

    feat: utilize 'HtmlInjectDllPlugin' to copy all dll files to the output folder

---
+++ src/configs/plugins/HtmlInjectDllPlugin.ts
@@ -0,0 +1,56 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type webpack from 'webpack';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const pluginName = 'HtmlInjectDllPlugin';
+
+interface Options {
+  projectOutputDirectory: string;
+  dllDirectory: string;
+  dllFileNamePrefix: string;
+}
+
+/** Copy the DLL file to the output folder and add the script tag to the HTML file. */
+export default class HtmlInjectDllPlugin {
+  projectOutputDirectory: string;
+  dllDirectory: string;
+  dllFileNamePrefix: string;
+
+  constructor(options: Options) {
+    this.projectOutputDirectory = options.projectOutputDirectory;
+    this.dllDirectory = options.dllDirectory;
+    this.dllFileNamePrefix = options.dllFileNamePrefix;
+  }
+
+  getDllFiles() {
+    const dllFiles = fs
+      .readdirSync(this.dllDirectory)
+      .filter((item) => item.startsWith(this.dllFileNamePrefix) && item.endsWith('.js'));
+
+    return dllFiles;
+  }
+
+  copyFile(dllFiles: string[]) {
+    try {
+      fs.statSync(this.projectOutputDirectory);
+    } catch (error) {
+      fs.mkdirSync(this.projectOutputDirectory);
+    }
+
+    for (const dllFile of dllFiles) {
+      fs.copyFileSync(path.resolve(this.dllDirectory, dllFile), path.resolve(this.projectOutputDirectory, dllFile));
+    }
+  }
+
+  apply(compiler: webpack.Compiler) {
+    compiler.hooks.compilation.tap(pluginName, (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).beforeAssetTagGeneration.tapAsync(pluginName, (data, cb) => {
+        const dllfiles = this.getDllFiles();
+        data.assets.js.unshift(...dllfiles);
+        this.copyFile(dllfiles);
+        cb(null, data);
+      });
+    });
+  }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,14 @@
 import path from 'node:path';
 import fs from 'node:fs';
 
-export const cacheDirectory = path.resolve(process.cwd(), '.temp_cache');
-export const outputDirectory = path.resolve(process.cwd(), 'dist');
-export const dllManifestPath = path.resolve(outputDirectory, 'vendor-manifest.json');
+const contextDirectory = process.cwd();
+
+export const projectOutputDirectory = path.resolve(contextDirectory, 'dist');
+
+export const tempDirectory = path.resolve(contextDirectory, '.temp');
+export const cacheDirectory = path.resolve(tempDirectory, '.temp_cache');
+export const dllDirectory = path.resolve(tempDirectory, '.dll');
+export const dllManifestPath = path.resolve(dllDirectory, 'vendor-manifest.json');
 
 export const projectPackageJson = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf-8')) as {
   version: string;
