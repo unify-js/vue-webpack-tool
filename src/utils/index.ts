11b9bf790342e23f25a0473009e3c6966282d380
luohuidong <luohuidong01@126.com>
2023-12-15 20:58:45 +0800

    refactor: optimize the cli code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import fs from 'node:fs';
 import ora from 'ora';
+import type webpack from 'webpack';
 
 const contextDirectory = process.cwd();
 
@@ -16,10 +17,55 @@ export const projectPackageJson = JSON.parse(fs.readFileSync(path.resolve(proces
   dependencies: Record<string, string>;
 };
 
-export function clearDir(options: { dir: string; startMessage: string; successMessage: string }) {
-  const { dir, startMessage, successMessage } = options;
+export function displayInfo(err: Error | undefined, stats: webpack.Stats | undefined) {
+  // https://webpack.js.org/api/node/#error-handling
+
+  // The err object will only contain webpack-related issues, such as misconfiguration
+  if (err) {
+    console.error(err.stack || err);
+
+    return;
+  }
+
+  if (stats) {
+    const info = stats.toJson();
+
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
+
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings);
+    }
+
+    console.log(stats.toString());
+  }
+}
+
+function clearDir(options: { dir: string; startMessage: string; endMessage: string }) {
+  const { dir, startMessage, endMessage } = options;
 
   const spinner = ora(startMessage).start();
   fs.rmSync(dir, { force: true, recursive: true });
-  spinner.succeed(successMessage);
+  spinner.succeed(endMessage);
+}
+
+export function clearDllFiles() {
+  clearDir({ dir: dllDirectory, startMessage: 'clearing old DLL files...', endMessage: 'old dll files cleared!' });
+}
+
+export function clearCacheFiles() {
+  clearDir({
+    dir: cacheDirectory,
+    startMessage: 'clearing old cache files...',
+    endMessage: 'old cache files cleared!',
+  });
+}
+
+export function clearOutputFiles() {
+  clearDir({
+    dir: projectOutputDirectory,
+    startMessage: 'clearing old output files...',
+    endMessage: 'old output files cleared!',
+  });
 }
