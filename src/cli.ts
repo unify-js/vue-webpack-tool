0c56021e607b1049940576bbbd717dfdaca6bdfb
luohuidong <luohuidong01@126.com>
2023-12-15 20:58:45 +0800

    refactor: optimize the cli code

--- src/cli.ts
+++ src/cli.ts
@@ -2,12 +2,10 @@
 
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import { Command, Option } from 'commander';
-import fs from 'node:fs';
+import { Command, Argument } from 'commander';
 import { merge } from 'webpack-merge';
-import ora from 'ora';
 
-import { cacheDirectory, projectOutputDirectory, projectPackageJson, dllDirectory, clearDir } from './utils/index.js';
+import { projectPackageJson, displayInfo, clearDllFiles, clearCacheFiles, clearOutputFiles } from './utils/index.js';
 import createWebpackDevConfig from './configs/webpack.dev.js';
 import createWebpackProdConfig from './configs/webpack.prod.js';
 import createWebpackDllConfig from './configs/webpack.dll.js';
@@ -19,7 +17,7 @@ program
   .version(projectPackageJson.version);
 
 program
-  .command('dev')
+  .command('start')
   .description('start dev server')
   .action(() => {
     const webpackDevConfig = createWebpackDevConfig();
@@ -31,31 +29,6 @@ program
     runServer();
   });
 
-function displayInfo(err: Error | undefined, stats: webpack.Stats | undefined) {
-  // https://webpack.js.org/api/node/#error-handling
-
-  // The err object will only contain webpack-related issues, such as misconfiguration
-  if (err) {
-    console.error(err.stack || err);
-
-    return;
-  }
-
-  if (stats) {
-    const info = stats.toJson();
-
-    if (stats.hasErrors()) {
-      console.error(info.errors);
-    }
-
-    if (stats.hasWarnings()) {
-      console.warn(info.warnings);
-    }
-
-    console.log(stats.toString());
-  }
-}
-
 program
   .command('build')
   .description('build for production')
@@ -66,29 +39,16 @@ program
 
 program
   .command('dll')
-  .description('manage DLL file')
-  .addOption(
-    new Option(
-      '-g, --generate <type>',
-      'generate DLL files. Pass "dev" to generate DLL files for development, pass "prod" to generate DLL files for production.'
-    ).choices(['dev', 'prod'])
-  )
-  .option('-c, --clear', 'clear the generated DLL files')
-  .action((options) => {
-    if (options.generate) {
-      webpack(
-        merge(createWebpackDllConfig(), {
-          mode: options.generate === 'prod' ? 'production' : 'development',
-        }),
-        displayInfo
-      );
-    }
-
-    if (options.clear) {
-      const spinner = ora('clearing old DLL files...').start();
-      fs.rmSync(dllDirectory, { force: true, recursive: true });
-      spinner.succeed('old dll files cleared!');
-    }
+  .description('generate DLL file')
+  .addArgument(new Argument('<mode>', 'generate DLL files for development or production').choices(['dev', 'prod']))
+  .action((arg) => {
+    clearDllFiles();
+    webpack(
+      merge(createWebpackDllConfig(), {
+        mode: arg === 'prod' ? 'production' : 'development',
+      }),
+      displayInfo
+    );
   });
 
 program
@@ -99,30 +59,9 @@ program
   .option('--dll', 'only clear dll files')
   .action((options) => {
     const all = Object.keys(options).length === 0;
-
-    if (all || options.cache) {
-      const spinner = ora('clearing old cache files...').start();
-      fs.rmSync(cacheDirectory, { force: true, recursive: true });
-      spinner.succeed('old cache files cleared!');
-    }
-
-    if (all || options.output) {
-      const spinner = ora('clearing old output files...').start();
-      fs.rmSync(projectOutputDirectory, {
-        force: true,
-        recursive: true,
-      });
-      spinner.succeed('old output files cleared !');
-    }
-
-    if (all || options.dll) {
-      const spinner = ora('clearing old dll files...').start();
-      fs.rmSync(dllDirectory, {
-        force: true,
-        recursive: true,
-      });
-      spinner.succeed('old dll files cleared!');
-    }
+    if (all || options.cache) clearCacheFiles();
+    if (all || options.output) clearOutputFiles();
+    if (all || options.dll) clearDllFiles();
   });
 
 program.parse();
