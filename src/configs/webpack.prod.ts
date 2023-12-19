18eccbf3ec8d54987ca1b4bf09ab423fa2fb9c4f
luohuidong <luohuidong01@126.com>
2023-12-20 00:02:42 +0800

    refactor: modify the optimization config to improve build performance

--- src/configs/webpack.prod.ts
+++ src/configs/webpack.prod.ts
@@ -3,5 +3,17 @@ import type webpack from 'webpack';
 export default function createWebpackProdConfig(): webpack.Configuration {
   return {
     mode: 'production',
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+    },
   };
 }
